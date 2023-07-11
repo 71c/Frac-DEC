700daefd6b3ab95c6a7a0db8c859af7d7c750674
Alon Jacobson <alojaco1@gmail.com>
2023-07-10 21:41:37 -0400

    update test_dd_0.m

--- matlab/test_dd_0.m
+++ matlab/test_dd_0.m
@@ -12,6 +12,7 @@ f = @(x, y, z) 4.*((-1/2)+x).^2+((-1/2)+y).^2+(-8).*x.*y.^2.*z+(-3).*cos(20.*(( 
 
 alpha_vec = [0.2 0.4 0.6 0.8];
 subdivisions_vec = [2 3 4 5 6 7 8 9 10];
+% subdivisions_vec = [2 4 8 16];
 
 errors_D1D0_method1 = zeros(length(subdivisions_vec), length(alpha_vec));
 errors_D1D0_method2 = zeros(length(subdivisions_vec), length(alpha_vec));
@@ -110,16 +111,36 @@ for subdiv_num = 1:length(subdivisions_vec)
         c2_method2 = D1_alpha * (D0_alpha * c0);
         c3_method2 = D2_alpha * (D1_alpha * c1);
 
-        errors_D1D0_method1(subdiv_num, alpha_num) = ((face_areas' * abs((1 ./ face_areas) .* c2_method1).^err_order) / sum(face_areas)) ^ (1/err_order);
-        errors_D2D1_method1(subdiv_num, alpha_num) = ((volumes' * abs((1 ./ volumes) .* c3_method1).^err_order) / sum(volumes)) ^ (1/err_order);
+        errors_D1D0_method1(subdiv_num, alpha_num) = mean(abs((1 ./ face_areas) .* c2_method1).^err_order)^(1/err_order);
+        errors_D2D1_method1(subdiv_num, alpha_num) = mean(abs((1 ./ volumes) .* c3_method1).^err_order)^(1/err_order);
 
-        errors_D1D0_method2(subdiv_num, alpha_num) = ((face_areas' * abs((1 ./ face_areas) .* c2_method2).^err_order) / sum(face_areas)) ^ (1/err_order);
-        errors_D2D1_method2(subdiv_num, alpha_num) = ((volumes' * abs((1 ./ volumes) .* c3_method2).^err_order) / sum(volumes)) ^ (1/err_order);
+        errors_D1D0_method2(subdiv_num, alpha_num) = mean(abs((1 ./ face_areas) .* c2_method2).^err_order)^(1/err_order);
+        errors_D2D1_method2(subdiv_num, alpha_num) = mean(abs((1 ./ volumes) .* c3_method2).^err_order)^(1/err_order);
     end
 end
 
-matrices = {errors_D1D0_method1, errors_D1D0_method2, errors_D2D1_method1, errors_D2D1_method2};
-titles = {'error D_1^\alpha D_0^\alpha method 1', 'error D_1^\alpha D_0^\alpha method 2', 'error D_2^\alpha D_1^\alpha method 1', 'error D_2^\alpha D_1^\alpha method 2'};
+% Test method 1 and 2
+% matrices = {errors_D1D0_method1, errors_D1D0_method2, errors_D2D1_method1, errors_D2D1_method2};
+% titles = {'error D_1^\alpha D_0^\alpha method 1', 'error D_1^\alpha D_0^\alpha method 2', 'error D_2^\alpha D_1^\alpha method 1', 'error D_2^\alpha D_1^\alpha method 2'};
+% for i = 1:length(matrices)
+%     title_i = titles{i};
+%     errors = matrices{i};
+% 
+%     figure
+%     hold on
+%     for j = 1:size(errors, 2)
+%         plots(j) = plot(subdivisions_vec, errors(:, j));
+%         names{j} = sprintf('\\alpha=%g', alpha_vec(j));
+%     end
+%     legend(plots, names)
+%     xlabel('subdivisions')
+%     ylabel('error')
+%     title(title_i);
+% end
+
+% Test only method 2
+matrices = {errors_D1D0_method2, errors_D2D1_method2};
+titles = {'error D_1^\alpha D_0^\alpha', 'error D_2^\alpha D_1^\alpha'};
 for i = 1:length(matrices)
     title_i = titles{i};
     errors = matrices{i};
@@ -133,10 +154,5 @@ for i = 1:length(matrices)
     legend(plots, names)
     xlabel('subdivisions')
     ylabel('error')
-    if weighted
-        title(sprintf('%s (weighted)', title_i));
-    else
-        title(sprintf('%s (unweighted)', title_i));
-    end
+    title(title_i);
 end
-
